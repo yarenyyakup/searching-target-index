ae31052b329d745d97cb11c6be92d31d38d7d802
Yaren Yakup <a.yarenyakup@gmail.com>
2022-10-26 23:12:42 +0300

    Add project files.

---
+++ searching target index/Program.cs
@@ -0,0 +1,32 @@
+﻿public class Solution
+{
+    public int Search(int[] nums, int target)
+    {
+
+
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (nums[i] == target)
+            {
+                return i;
+
+            }
+
+
+
+        }
+
+
+        return -1;
+
+    }
+
+    public static void Main(string[] args)
+    {
+        int[] str = new int[] { -1, 0, 3, 5, 9, 12 };
+        int x = 7;
+        Solution s = new Solution();
+        Console.WriteLine(s.Search(str, x));
+    }
+}
